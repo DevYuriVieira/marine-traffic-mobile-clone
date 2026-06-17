ee8b16a8281012cd96ad0783dbebe574589a7c7e
Yuri <yurivieirateixeira@hotmail.com>
2026-06-17 01:38:32 -0300

    feat: setup application routing with AppRoutes

---
+++ src/routers/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from '../pages/Login';
+import Home from '../pages/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false, 
+      }}
+    >
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
